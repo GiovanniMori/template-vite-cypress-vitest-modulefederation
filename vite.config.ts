6262abcd8f12573cf7fbdf1ef996d70824e4d8f6
Giovanni Mori <giovannimori@MacBook-Air-de-Giovanni.local>
2024-01-04 15:33:20 -0300

    setup

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
